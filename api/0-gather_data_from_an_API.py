c9e7486d198b31c1d85a77ef37c645c9f21a00a5
daggzu <diegog8603@gmail.com>
2023-12-21 20:37:31 -0400

    checkers

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module returns information about his/her TO_DO list progress
-when an employee ID is provided as argument.
+when an employee ID is provided as an argument.
 """
 import requests
 import sys
@@ -17,25 +17,22 @@ def get_employee_todo_progress(employee_id):
     user_data = user_response.json()  # Parse user names to JSON
     employee_name = user_data['name']
 
-
-#  Get the user's tasks through the API
+    # Get the user's tasks through the API
     todo_response = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
             employee_id))
     todo_data = todo_response.json()  # Parse user's tasks to JSON
     total_tasks = len(todo_data)  # Get the total number of tasks
 
-
-# Get the number of completed tasks
+    # Get the number of completed tasks
     done_tasks = [task for task in todo_data if task['completed'] is True]
     num_done_tasks = len(done_tasks)  # Get  the number of completed tasks
 
-# Print the user's tasks
+    # Print the user's tasks
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, num_done_tasks, total_tasks))
     for task in done_tasks:
-        print("\t {}".format(task['title']))
-
+        print("\t{}".format(task['title']))  # Adjusted indentation
 
 if __name__ == '__main__':
     employee_id = int(sys.argv[1])
