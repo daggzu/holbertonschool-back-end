6d2d4c15b8bad0ab684441349013340cadd0b2c9
daggzu <diegog8603@gmail.com>
2023-12-22 10:14:20 -0400

    im here to get points

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Module returns information about his/her TO_DO list progress
+when an employee ID is provided as argument.
+"""
+import csv
+import requests
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    """
+    Returns information about his/her TODO list progress.
+    """
+    # Get the user name through the API
+    user_response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    user_data = user_response.json()  # Parse user names to JSON
+    employee_name = user_data['name']
+
+
+#  Get the user's tasks through the API
+    todo_response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            employee_id))
+    todo_data = todo_response.json()  # Parse user's tasks to JSON
+    total_tasks = len(todo_data)  # Get the total number of tasks
+
+
+# Get the number of completed tasks
+    done_tasks = [task for task in todo_data if task['completed'] is True]
+    num_done_tasks = len(done_tasks)  # Get  the number of completed tasks
+
+# Print the user's tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, num_done_tasks, total_tasks))
+    for task in done_tasks:
+        print("\t {}".format(task['title']))
+
+
+# Task 1: Export to CSV
+    with open('{}.csv'.format(employee_id), 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo_data:
+            writer.writerow([employee_id, employee_name,
+                             task['completed'], task['title']])
+# Task 1 (End)
+
+
+if __name__ == '__main__':
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
