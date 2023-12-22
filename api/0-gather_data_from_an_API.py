60c5672c9020bdf41fcf442b05c3969ad84d7e96
daggzu <diegog8603@gmail.com>
2023-12-21 20:55:56 -0400

    SUFRIENDO

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,44 +1,42 @@
 #!/usr/bin/python3
 """
-    data gathering from api module
+Module returns information about his/her TO_DO list progress
+when an employee ID is provided as argument.
 """
 import requests
 import sys
 
-# python3 0-gather_data_from_an_API.py 2 is the run command where two is the id
-employee_id = sys.argv[1]
 
-# uses request import to get the data from the api
-user_response = requests.get(
-    'https://jsonplaceholder.typicode.com/users/' + employee_id)
+def get_employee_todo_progress(employee_id):
+    """
+    Returns information about his/her TODO list progress.
+    """
+    # Get the user name through the API
+    user_response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    user_data = user_response.json()  # Parse user names to JSON
+    employee_name = user_data['name']
 
-# converts the data to json format
-data = user_response.json()
 
-# gets the name of the employee
-employee_name = data['name']
+#  Get the user's tasks through the API
+    todo_response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            employee_id))
+    todo_data = todo_response.json()  # Parse user's tasks to JSON
+    total_tasks = len(todo_data)  # Get the total number of tasks
 
-# gets the tasks of the employee
-todo_response = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
 
-# converts the tasks data to json format
-todo_data = todo_response.json()
+# Get the number of completed tasks
+    done_tasks = [task for task in todo_data if task['completed'] is True]
+    num_done_tasks = len(done_tasks)  # Get  the number of completed tasks
 
-# gets the total number of tasks
-todo_total = str(len(todo_data))
+# Print the user's tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, num_done_tasks, total_tasks))
+    for task in done_tasks:
+        print("\t {}".format(task['title']))
 
-# gets the number of completed tasks
-todo_completed = str(sum(1 for task in todo_data if task['completed']))
-
-# prints the data in the format required
-print('Employee {} is done with tasks({}/{}):'.format(employee_name,
-      todo_completed, todo_total))
-
-# prints the completed tasks by title of task
-for task in todo_data:
-    if task['completed']:
-        print('\t {}'.format(task['title']))
 
 if __name__ == '__main__':
-    pass
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
