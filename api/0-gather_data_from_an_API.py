0e8abd79eaf267b4ea850757b77575d639c2853d
daggzu <diegog8603@gmail.com>
2023-12-22 10:09:00 -0400

    my lawd

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
-"""
-Module returns information about his/her TO_DO list progress
-when an employee ID is provided as argument.
-"""
+"""Script to use a REST API for a given employee ID, returns
+information about his/her TODO list progress"""
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Returns information about his/her TODO list progress.
-    """
-    # Get the user name through the API
-    user_response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-    user_data = user_response.json()  # Parse user names to JSON
-    employee_name = user_data['name']
+if name == "main":
+    if len(sys.argv) != 2:
+        print(f"UsageError: python3 {file} employee_id(int)")
+        sys.exit(1)
 
+    API_URL = "https://jsonplaceholder.typicode.com/"
+    EMPLOYEE_ID = sys.argv[1]
 
-#  Get the user's tasks through the API
-    todo_response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-            employee_id))
-    todo_data = todo_response.json()  # Parse user's tasks to JSON
-    total_tasks = len(todo_data)  # Get the total number of tasks
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
+    data = response.json()
 
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
 
-# Get the number of completed tasks
-    done_tasks = [task for task in todo_data if task['completed'] is True]
-    num_done_tasks = len(done_tasks)  # Get  the number of completed tasks
+    employee_name = data[0]["user"]["name"]
+    total_tasks = len(data)
+    done_tasks = [task for task in data if task["completed"]]
+    total_done_tasks = len(done_tasks)
 
-# Print the user's tasks
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, num_done_tasks, total_tasks))
+    print(f"Employee {employee_name} is done with tasks"
+          f"({total_done_tasks}/{total_tasks}):")
     for task in done_tasks:
-        print("\t {}".format(task['title']))
-
-
-if __name__ == '__main__':
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+        print(f"\t {task['title']}")
